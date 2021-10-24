1541d10520d8a51d120597d4b8a073c636a4936e
Aristotel <92230633+aristotel-tuktuk@users.noreply.github.com>
2021-10-24 12:39:51 +0300

    Add files via upload

---
+++ exersice13.py
@@ -0,0 +1,11 @@
+# Задача «Минимум из трех чисел»
+# Даны три целых числа. Выведите значение наименьшего из них.
+a = int(input())
+b = int(input())
+c = int(input())
+if b >= a <= c:
+    print(a)
+elif a >= b <= c:
+    print(b)
+else:
+    print(c)
